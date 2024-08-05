12a51dfea9cd0448c6161bc39d773e7b8d8ea756
Dustymon111 <dasutinraioneru@gmail.com>
2024-08-05 19:48:36 +0700

    update port

--- main.py
+++ main.py
@@ -135,4 +135,4 @@ def hello():
     return "Hello, New World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
